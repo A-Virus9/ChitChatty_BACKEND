ac9bc479103e3e809f5bf7a532ca4c1614751239
VirusSir <vyasakshat123@gmail.com>
2025-03-20 22:44:07 +0530

    test push

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const morgan = require("morgan");
 const userRouter = require("./routers/userRouter.js");
 const chatRouter = require("./routers/chatRouter.js");
 const createError = require("./utils/createError");
-const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
@@ -21,13 +20,6 @@ app.use(helmet());
 
 app.use(morgan("dev"));
 
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 10000,
-  message: "Too many requests from this IP, please try again in an hour",
-});
-
-app.use("/", limiter);
 app.use(express.json({ limit: "10kb" }));
 app.use(mongoSanitize());
 app.use(xss());
