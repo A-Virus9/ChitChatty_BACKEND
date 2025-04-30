74950638538b9dd6abc10200421efe1133c77387
VirusSir <vyasakshat123@gmail.com>
2025-04-30 10:28:31 +0530

    test

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -64,7 +64,7 @@ exports.createUser = catchAsync(async (req, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: false,
+    httpOnly: true,
     secure: true,
     sameSite: "None"
   });
@@ -96,7 +96,7 @@ exports.loginUser = catchAsync(async (req, res, next) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: false,
+    httpOnly: true,
     secure: true,
     sameSite: "None"
   });
